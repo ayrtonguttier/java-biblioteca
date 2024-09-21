80a9f745e7b16334b8700519c5131d908e712da0
Ayrton dos Santos Guttier <ayrtonsg@hotmail.com>
2024-09-21 19:18:38 -0300

    first

---
+++ src/main/java/Program.java
@@ -0,0 +1,8 @@
+public class Program{
+    public static void main(String[] args){
+        System.out.println("Olá mundo");
+        for(String arg : args){
+            System.out.println(arg);
+        }
+    }
+}
---
+++ src/main/java/catalogo/Livro.java
@@ -0,0 +1,4 @@
+package br.com.ayrtonguttier.biblioteca.catalogo;
+public class Livro{
+    private String titulo;
+}
