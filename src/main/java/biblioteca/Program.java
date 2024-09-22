3de3cb32a39322992edf79aac3ac09a501cae3af
Ayrton dos Santos Guttier <ayrtonsg@hotmail.com>
2024-09-22 13:41:10 -0300

    docker compose build application

--- src/main/java/biblioteca/Program.java
+++ src/main/java/biblioteca/Program.java
@@ -3,10 +3,14 @@ package br.com.ayrtonguttier.biblioteca;
 import org.postgresql.*;
 import br.com.ayrtonguttier.biblioteca.catalogo.livro.*;
 import java.util.ArrayList;
+import java.util.logging.*;
 
 public class Program{
     public static void main(String[] args){
         try{
+            Logger logger = Logger.getLogger(Program.class.getName());
+            logger.info("Iniciando processamento");
+
             Repository r = new Repository();
             ArrayList<Livro> livros = r.getAll();
             for(Livro l : livros){
